3d9105015203232d33d04fd82b685d7e86d52187
Daniel <dlu0@sunrise.ch>
2026-03-21 09:22:54 +0100

    Add files via upload

--- yt6801-autoinstall.sh
+++ yt6801-autoinstall.sh
@@ -19,17 +19,19 @@
 # Da Cron eingeschränkten PATH
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 #################### --- Konfiguration --- ####################
 
-DEB_PKG1="/root/tuxedo-yt6801_1.0.28-1_all.deb"
-DEB_PKG2="/root/tuxedo-yt6801_1.0.30tux5_all.deb"
+DEB_PKG1="$SCRIPT_DIR/tuxedo-yt6801_1.0.28-1_all.deb"
+DEB_PKG2="$SCRIPT_DIR/tuxedo-yt6801_1.0.30tux5_all.deb"
 
 PING_TARGET="192.168.1.1"                             # !!! Hier Router/Gateway IP eintragen !!!
 
 ####
 
 LOGFILE="/var/log/yt6801-autoinstall.log"
-MARKER="/root/yt6801_stage"
+MARKER="$SCRIPT_DIR/yt6801_stage"
 
 ############ --- Ab hier keine Änderungen mehr --- ############
 
@@ -61,10 +63,6 @@ for arg in "$@"; do
     esac
 done
 
-if [ "$DEBUG" = true ]; then
-    log "DEBUG aktiviert"
-fi
-
 mkdir -p /var/log
 touch "$LOGFILE"
 
@@ -77,10 +75,38 @@ STAGE=$(cat "$MARKER")
 log ""
 log "=== Start yt6801 Auto-Installer ==="
 
-sleep 10
+NETWORK_OK=false
+
+for i in {1..10}; do
+    log "Warte auf Netzwerk... Versuch $i/10"
+    if ping -c 1 -W 1 "$PING_TARGET" &>/dev/null; then
+        log "Netzwerk erreichbar nach $i Versuch(en)"
+        NETWORK_OK=true
+        break
+    fi
+    sleep 1
+done
+
+if [[ $NETWORK_OK == false ]]; then
+    log "Netzwerk nach 10 Versuchen NICHT erreichbar!"
+fi
+
+if [ "$DEBUG" = true ]; then
+    log "DEBUG aktiviert"
+    if [ -f "$DEB_PKG1" ]; then
+        log "Deb-Paket $DEB_PKG1 gefunden :D"
+    else
+        log "Deb-Paket $DEB_PKG1 nicht gefunden!"
+    fi
+    if [ -f "$DEB_PKG2" ]; then
+        log "Deb-Paket $DEB_PKG2 gefunden :D"
+    else
+        log "Deb-Paket $DEB_PKG2 nicht gefunden!"
+    fi
+fi
 
 # --- Netzwerk testen ---
-if ping -c 1 -W 2 $PING_TARGET &>/dev/null; then
+if [[ $NETWORK_OK == true ]]; then
     log "Netzwerk funktioniert, keine Aktion notwendig."
     echo 0 > "$MARKER"
     exit 0
@@ -98,7 +124,12 @@ log "Stage: $STAGE"
 if [ "$STAGE" -eq 1 ]; then
     DEB_PKG="$DEB_PKG1"
 elif [ "$STAGE" -eq 2 ]; then
-    DEB_PKG="$DEB_PKG2"
+    if [ -f "$DEB_PKG2" ]; then
+        DEB_PKG="$DEB_PKG2"
+    else
+        log "Fallback: DEB_PKG2 nicht vorhanden, verwende DEB_PKG1 erneut"
+        DEB_PKG="$DEB_PKG1"
+    fi
 else
     log "Keine Treiberinstallation erfolgreich!"
     exit 1
@@ -112,6 +143,8 @@ if [ -f "$DEB_PKG" ]; then
             log "dpkg-Installation fehlgeschlagen!"
             exit 1
         fi
+    else
+        log "DEBUG: Keine Installation"
     fi
 else
     log "Deb-Paket $DEB_PKG nicht gefunden!"
@@ -134,6 +167,8 @@ if lsmod | grep -q yt6801; then
 elif [ "$DEBUG" = false ]; then
     log "Lade Kernel-Modul yt6801 ..."
     modprobe yt6801 >> "$LOGFILE" 2>&1
+else
+    log "DEBUG: modprobe yt6801 nicht ausgeführt"
 fi
 
 # --- Anzeige ---
@@ -145,6 +180,7 @@ log "Treiber ist geladen. reboot..."
 if [ "$DEBUG" = false ]; then
     reboot
 else
-    log "DEBUG: Kein Reboot."
+    log "DEBUG: Kein Reboot. Stage wieder auf 0 gestellt."
+    echo 0 > "$MARKER"
 fi
 
